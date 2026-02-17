2ab426cb5429c0c8faacb1fc51577705bdb2edc3
opticaldrive <138826219+opticaldrive@users.noreply.github.com>
2026-02-16 17:35:49 -0800

    no

--- main.py
+++ main.py
@@ -13,22 +13,17 @@ SLACK_USER_COOKIE = os.getenv("SLACK_USER_COOKIE")
 print(SLACK_BOT_TOKEN, SLACK_USER_COOKIE, SLACK_USER_TOKEN)
 
 
-WS_URL = ()
-
-HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:147.0) Gecko/20100101 Firefox/147.0",
-    "Origin": "https://app.slack.com",
-    "Cookie": f"d={SLACK_USER_COOKIE}",
-}
-
-
 async def enable_ear():
     async with websockets.connect(WS_URL, additional_headers=HEADERS) as ws:
         hello = await ws.recv()
         print(f"Connected: {json.loads(hello).get('type')}", flush=True)
         async for message in ws:
             data = json.loads(message)
-            print(data)
+            # print(data)
+            print(data["type"])  # , data["subtype"])
+            if data["type"] == "user_typing":
+                # print(data)
+                print(data["channel"], data["user"], data.get("thread_ts", ""))
 
 
 if __name__ == "__main__":
